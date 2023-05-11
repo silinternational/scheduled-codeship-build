b9838f80750a69cc742105bc0adae048f6fa625b
Schparky <3172830+Schparky@users.noreply.github.com>
2023-05-11 08:35:05 +0800

    more test checks

--- cron/builder/main_test.go
+++ cron/builder/main_test.go
@@ -1,24 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestUnmarshalProjectList(t *testing.T) {
-	good := `[{"uuid":"26e97136-8265-4172-867d-3392c7b3c322","ref":"20.04"}]`
+	id := "26e97136-8265-4172-867d-3392c7b3c322"
+	ref := "20.04"
+	good := fmt.Sprintf(`[{"uuid":"%s","ref":"%s"}]`, id, ref)
 	l, err := unmarshalProjectList(good)
-	if err != nil {
-		t.Errorf("unexpected error: %s", err)
-	}
-	if len(l) != 1 {
-		t.Errorf("expected a list of projects, got %s", l)
-	}
+	require.NoError(t, err)
+	require.Equal(t, 1, len(l), "expected a list of projects, got %s", l)
+	require.Equal(t, id, l[0].UUID)
+	require.Equal(t, ref, l[0].Ref)
 
 	bad := `[{"uuid":"26e97136-8265-4172-867d-3392c7b3c322","ref":"notice the trailing comma ->"},]`
 	l, err = unmarshalProjectList(bad)
-	if err == nil {
-		t.Errorf("expected an error but got nil")
-	}
+	require.Error(t, err)
 }
 
 // To run this test locally, first ensure you have all the required
