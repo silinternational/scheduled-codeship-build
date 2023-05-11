94907a8908d7c9737cc2771e601a271985ed8d27
Schparky <3172830+Schparky@users.noreply.github.com>
2023-05-11 11:25:10 +0800

    access key shouldn't be nonsensitive

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,8 +13,9 @@ module "serverless-user" {
 output "serverless-access-key-id" {
   value = module.serverless-user.aws_access_key_id
 }
+
 output "serverless-secret-access-key" {
-  value = nonsensitive(module.serverless-user.aws_secret_access_key)
+  value = module.serverless-user.aws_secret_access_key
 }
 
 resource "aws_ssm_parameter" "username" {
